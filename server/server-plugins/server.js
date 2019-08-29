dfc02ab333b1052d38c533802531cde20e84d4ff
Volco <13051425+Volco@users.noreply.github.com>
2019-08-28 20:12:46 -0400

    Update server.js

--- server/server-plugins/server.js
+++ server/server-plugins/server.js
@@ -3,7 +3,7 @@
 const FS = require("../../.lib-dist/fs").FS;
 const https = require("https");
 const http = require("http");
-const Pokedex = require("../data/pokedex.js").BattlePokedex;
+const Pokedex = require("../../data/pokedex.js").BattlePokedex;
 
 const bubbleLetterMap = new Map([
 	["a", "\u24D0"], ["b", "\u24D1"], ["c", "\u24D2"], ["d", "\u24D3"], ["e", "\u24D4"], ["f", "\u24D5"], ["g", "\u24D6"], ["h", "\u24D7"], ["i", "\u24D8"], ["j", "\u24D9"], ["k", "\u24DA"], ["l", "\u24DB"], ["m", "\u24DC"],
