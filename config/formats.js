063d1b73036297a614865f41b2850fccc58a5cfc
Zach Hartin <zach.hartin@gmail.com>
2019-09-09 16:09:41 -0400

    Update formats.js

--- config/formats.js
+++ config/formats.js
@@ -2197,7 +2197,7 @@ let Formats = [
 		],
 		mod: 'franticfusions',
 		ruleset: ['Sleep Clause Mod', 'Species Clause', 'OHKO Clause', 'Moody Clause', 'Evasion Moves Clause', 'Endless Battle Clause', 'HP Percentage Mod', 'Cancel Mod', 'Team Preview'],
-		banlist: ['Uber', 'Unreleased', 'Shadow Tag', 'Assist', 'Shedinja', 'Medichamite', 'Kyurem-Black'],
+		banlist: ['Uber', 'Unreleased', 'Shadow Tag', 'Assist', 'Shedinja', 'Medichamite', 'Kyurem-Black', 'Serperior'],
 		unbanlist: ['Deoxys-Speed', 'Deoxys-Defense', 'Darkrai', 'Genesect', 'Aegislash'],
 		onModifyTemplate(template, pokemon) {
 			let fusionTemplate = this.getTemplate(pokemon.name), mixedTemplate = Object.assign({}, template);
