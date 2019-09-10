1186974a3c3909159870a7474ddc07784d8ba100
Insist <DeathlyPlays@users.noreply.github.com>
2019-09-09 23:30:40 -0500

    SSSB: Use main's SSB randomization format

--- data/mods/sssb/random-teams.js
+++ data/mods/sssb/random-teams.js
@@ -1,10 +1,28 @@
 'use strict';
 
+/** @typedef {{[name: string]: SSBSet}} SSBSets */
+/**
+ * @typedef {Object} SSBSet
+ * @property {string} species
+ * @property {string | string[]} ability
+ * @property {string | string[]} item
+ * @property {GenderName} gender
+ * @property {(string | string[])[]} moves
+ * @property {string} signatureMove
+ * @property {{hp?: number, atk?: number, def?: number, spa?: number, spd?: number, spe?: number}=} evs
+ * @property {{hp?: number, atk?: number, def?: number, spa?: number, spd?: number, spe?: number}=} ivs
+ * @property {string | string[]} nature
+ * @property {number=} level
+ * @property {boolean=} shiny
+ */
+
 const RandomTeams = require('../../random-teams');
 
 class RandomSSSBTeams extends RandomTeams {
 	randomSSSBTeam() {
+		/** @type {PokemonSet[]} */
 		let team = [];
+		/** @type {SSBSets} */
 		let sets = {
 			// Breaking Gods
 			"☢RaginInfernape": {
@@ -30,28 +48,64 @@ class RandomSSSBTeams extends RandomTeams {
 			// Drivers
 			// Voices
 		};
-		// convert moves to ids.
-		for (let k in sets) {
-			sets[k].moves = sets[k].moves.map(toID);
-			sets[k].baseSignatureMove = toID(sets[k].baseSignatureMove);
-		}
-
-		// Generate the team randomly.
-		let pool = this.shuffle(Object.keys(sets));
-		for (let i = 0; i < 6; i++) {
-			let set = sets[pool[i]];
-			set.level = 100;
-			set.name = pool[i];
-			if (!set.ivs) {
-				set.ivs = {hp: 31, atk: 31, def: 31, spa: 31, spd: 31, spe: 31};
-			} else {
-				for (let iv in {hp: 31, atk: 31, def: 31, spa: 31, spd: 31, spe: 31}) {
-					set.ivs[iv] = set.ivs[iv] || set.ivs[iv] === 0 ? set.ivs[iv] : 31;
+		let pool = Object.keys(sets);
+		/** @type {{[type: string]: number}} */
+		let typePool = {};
+		while (pool.length && team.length < 6) {
+			let name = this.sampleNoReplace(pool);
+			let ssbSet = sets[name];
+			// Enforce typing limits
+			let types = this.getTemplate(ssbSet.species).types;
+			let rejected = false;
+			for (let type of types) {
+				if (typePool[type] === undefined) typePool[type] = 0;
+				if (typePool[type] >= 2) {
+					// Reject
+					rejected = true;
+					break;
 				}
 			}
-			// Assuming the hardcoded set evs are all legal.
-			if (!set.evs) set.evs = {hp: 84, atk: 84, def: 84, spa: 84, spd: 84, spe: 84};
-			set.moves = [this.sampleNoReplace(set.moves), this.sampleNoReplace(set.moves), this.sampleNoReplace(set.moves)].concat(set.signatureMove);
+			if (rejected) continue;
+			// Update type counts
+			for (let type of types) {
+				typePool[type]++;
+			}
+			/** @type {PokemonSet} */
+			let set = {
+				name: name,
+				species: ssbSet.species,
+				item: Array.isArray(ssbSet.item) ? this.sampleNoReplace(ssbSet.item) : ssbSet.item,
+				ability: Array.isArray(ssbSet.ability) ? this.sampleNoReplace(ssbSet.ability) : ssbSet.ability,
+				moves: [],
+				nature: Array.isArray(ssbSet.nature) ? this.sampleNoReplace(ssbSet.nature) : ssbSet.nature,
+				gender: ssbSet.gender,
+				evs: {hp: 0, atk: 0, def: 0, spa: 0, spd: 0, spe: 0},
+				ivs: {hp: 31, atk: 31, def: 31, spa: 31, spd: 31, spe: 31},
+				level: ssbSet.level || 100,
+				shiny: ssbSet.shiny,
+			};
+			if (ssbSet.ivs) {
+				for (let iv in ssbSet.ivs) {
+					// IVs from the set override the default of 31, assume the hardcoded IVs are legal
+					// @ts-ignore StatsTable has no index signature
+					set.ivs[iv] = ssbSet.ivs[iv];
+				}
+			}
+			if (ssbSet.evs) {
+				for (let ev in ssbSet.evs) {
+					// EVs from the set override the default of 0, assume the hardcoded EVs are legal
+					// @ts-ignore StatsTable has no index signature
+					set.evs[ev] = ssbSet.evs[ev];
+				}
+			} else {
+				set.evs = {hp: 84, atk: 84, def: 84, spa: 84, spd: 84, spe: 84};
+			}
+			while (set.moves.length < 3 && ssbSet.moves.length > 0) {
+				let move = this.sampleNoReplace(ssbSet.moves);
+				if (Array.isArray(move)) move = this.sampleNoReplace(move);
+				set.moves.push(move);
+			}
+			set.moves.push(ssbSet.signatureMove);
 			team.push(set);
 		}
 		return team;
