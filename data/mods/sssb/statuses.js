00442f36202ea16e5c6434fb6709527fe307a033
Insist <DeathlyPlays@users.noreply.github.com>
2019-09-09 22:33:39 -0500

    SSSB: Add RaginInfernape's Promotion

--- data/mods/sssb/statuses.js
+++ data/mods/sssb/statuses.js
@@ -5,13 +5,13 @@ let BattleStatuses = {
 	ragininfernape: {
 		noCopy: true,
 		onStart() {
-			this.add(`c|@RaginInfernape|I'm just here procrastinating so let's get this bread gamers.  The server being killed with crashes is just a nice meme anyways.`);
+			this.add(`c|☢RaginInfernape|I'm just here procrastinating so let's get this bread gamers.  The server being killed with crashes is just a nice meme anyways.`);
 		},
 		onSwitchOut() {
-			this.add(`c|@RaginInfernape|I guess I should finally get to work on those errors.`);
+			this.add(`c|☢RaginInfernape|I guess I should finally get to work on those errors.`);
 		},
 		onFaint() {
-			this.add(`c|@RaginInfernape|Well, now I'm dead... That's the perfect excuse to not fix that yet.`);
+			this.add(`c|☢RaginInfernape|Well, now I'm dead... That's the perfect excuse to not fix that yet.`);
 		},
 	},
 };
