9e6c6827f3072b6e64d6a605bd83860afb516128
Insist <DeathlyPlays@users.noreply.github.com>
2019-09-09 19:20:21 -0500

    SSSB: Add RaginInfernape's Custom Move

---
+++ data/mods/sssb/moves.js
@@ -0,0 +1,29 @@
+"use strict";
+
+/**@type {{[k: string]: MoveData}} */
+let BattleMovedex = {
+	// RaginInfernape
+	"fireydestruction": {
+		id: "fireydestruction",
+		name: "Firey Destruction",
+		basePower: 100,
+		accuracy: true,
+		category: "Physical",
+		shortDesc: "50% chance to burn.",
+		desc: "50% chance to burn the opponent.",
+		pp: 15,
+		secondary: {
+			chance: 50,
+			status: "brn",
+		},
+		onPrepareHit(target, source) {
+			this.add('-anim', source, 'Fiery Dance', source);
+			this.add('-anim', source, 'Flare Blitz', source);
+		},
+		priority: 0,
+		target: "normal",
+		type: "Fire",
+	},
+};
+
+exports.BattleMovedex = BattleMovedex;
