9e05ece8fba27d9d228217c219c12d716cf5f9e5
Zach Hartin <zach.hartin@gmail.com>
2019-09-11 15:28:44 -0400

    Add Fakemons Folder + pokedex.js

---
+++ data/mods/fakemons/pokedex.js
@@ -0,0 +1,32 @@
+'use strict';
+
+/**@type {{[k: string]: TemplateData}} */
+let BattlePokedex = {
+	talow: {
+		num: 1,
+		species: "Talow",
+		types: ["Flying"],
+		baseStats: {hp: 40, atk: 60, def: 32, spa: 50, spd: 36, spe: 82},
+		abilities: {0: "Keen Eye", H: "Poison Heal"},
+		heightm: 0.3,
+		weightkg: 3.4,
+		color: "Green",
+		evos: ["talograw"],
+		eggGroups: ["Flying"],
+	},
+  talograw: {
+		num: 2,
+		species: "Talograw",
+		types: ["Flying", "Poison"],
+		baseStats: {hp: 70, atk: 112, def: 73, spa: 98, spd: 80, spe: 126},
+		abilities: {0: "Intimidate", H: "Poison Heal"},
+		heightm: 1.1,
+		weightkg: 15,
+		color: "Purple",
+		prevo: "talow",
+		evoLevel: 16,
+		eggGroups: ["Flying", "Monster"],
+	},
+};
+
+exports.BattlePokedex = BattlePokedex;
