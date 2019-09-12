1d46fb7ac97bd769a2c5289d9ce28c4536d523c5
Zach Hartin <zach.hartin@gmail.com>
2019-09-12 13:02:23 -0400

    Add formats-data (Fakemons)

---
+++ data/mods/fakemons/formats-data.js
@@ -0,0 +1,35 @@
+'use strict';
+
+/**@type {{[k: string]: TemplateFormatsData}} */
+let BattleFormatsData = {
+	talograw: {
+		randomBattleMoves: ["gunkshot", "drillrun", "aerialace", "beakblast", "sludgewave", "defog", "tailwind", "toxicspikes", "flamecharge"],
+		tier: "OU",
+	},
+	frostorus: {
+		randomBattleMoves: ["calmmind", "icebeam", "thunderbolt", "freezedry", "defog", "iceshard", "taunt", "uturn", "focusblast", "blizzard"],
+		tier: "OU",
+	},
+  frostorustherian: {
+		randomBattleMoves: ["icebeam", "focusblast", "thunderbolt", "uturn", "calmmind", "blizzard", "hyperbeam", "uproar", "taunt", "defog"],
+		tier: "OU",
+	},
+  semighoul: {
+		randomBattleMoves: ["hypervoice", "shadowball", "willowisp", "aurasphere", "destinybond", "perishsong", "protect", "partingshot"],
+		tier: "OU",
+	},
+  dynamouse: {
+		randomBattleMoves: ["superfang", "hypervoice", "fireblast", "explosion", "glare", "meanlook", "willowisp", "hiddenpowergrass", "flamethrower"],
+		tier: "OU",
+	},
+  moshka: {
+		randomBattleMoves: ["thunderwave", "dazzlinggleam", "shadowball", "destinybond", "hypervoice", "hex", "calmmind", "substitute", "psychic", "trick", "torment", "doubleteam", "psyshock", "disarmingvoice"],
+		tier: "OU",
+	},
+  protokkol: {
+		randomBattleMoves: ["trickroom", "flashcannon", "hyperbeam", "thunderbolt", "hiddenpowerfire", "gyroball", "signalbeam", "voltswitch", "irondefense", "stealthrock"],
+		tier: "OU",
+	},
+};
+
+exports.BattleFormatsData = BattleFormatsData;
