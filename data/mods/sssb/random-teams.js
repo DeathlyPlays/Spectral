3c10e552080216bc84f38d96f03906309f859fcc
Bamd <bamdthebamd@gmail.com>
2019-09-14 10:30:12 +0600

    SSSB: Promote BAMD to % (#23)

--- data/mods/sssb/random-teams.js
+++ data/mods/sssb/random-teams.js
@@ -88,8 +88,7 @@ class RandomSSSBTeams extends RandomTeams {
 			// Bots
 			// Players
 			// Drivers
-			// Voices
-			"+Back At My Day": {
+			"%Back At My Day": {
 				species: "Zapdos",
 				item: "Life Orb",
 				ability: "Peal of Thunder",
@@ -103,6 +102,7 @@ class RandomSSSBTeams extends RandomTeams {
 				},
 				nature: "Timid",
 			},
+			// Voices
 		};
 		let pool = Object.keys(sets);
 		/** @type {{[type: string]: number}} */
