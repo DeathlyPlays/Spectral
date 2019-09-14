6134c5aa117017dabcd8816cc7463a220de3631d
Diler <diler.vinho@outlook.com>
2019-09-14 01:04:41 -0300

    SSB: Add Zakuree and Chandie's Item and Chandie's Ability (#21)
    * Update items.js
    
    * SSSB: Fix formatting
    
    * SSSB: Add Zakuree's Soul Orb
    
    * SSSB: Add Chandie's Ability

--- data/mods/sssb/items.js
+++ data/mods/sssb/items.js
@@ -12,6 +12,33 @@ let BattleItems = {
 		zMoveUser: ["Arcanine"],
 		desc: "If held by an Arcanine with Meteor Charge, it can use Eternal Flames.",
 	},
+
+	"voidheart": {
+		id: "voidheart",
+		name: "Void Heart",
+		spritenum: 689,
+		onTakeItem: false,
+		zMove: "Embrace the Void",
+		zMoveFrom: "Sharp Shadow",
+		zMoveUser: ["Marshadow"],
+		gen: 7,
+		desc: "If held by a Marshadow with Sharp Shadow, it can use Embrace the Void.",
+	},
+
+	"soulorb": {
+		id: "soulorb",
+		name: "Soul Orb",
+		spritenum: 249,
+		onModifyDamage(damage, source, target, move) {
+			return this.chainModify(1.5);
+		},
+		onAfterMoveSecondarySelf(source, target, move) {
+			if (source && source !== target && move && move.category !== 'Status') {
+				this.damage(source.maxhp / 10, source, source, this.getItem('soulorb'));
+			}
+		},
+		desc: "Holder's attacks do 1.5x damage, and it loses 1/10 its max HP after the attack.",
+	},
 };
 
 exports.BattleItems = BattleItems;
