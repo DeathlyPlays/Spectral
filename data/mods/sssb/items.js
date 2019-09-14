bb515f2445f4eecf75838c4005dc3e2d9c0d93fd
Insist <DeathlyPlays@users.noreply.github.com>
2019-09-13 22:38:06 -0500

    SSSB: Add Zakuree's Soul Orb

--- data/mods/sssb/items.js
+++ data/mods/sssb/items.js
@@ -12,6 +12,7 @@ let BattleItems = {
 		zMoveUser: ["Arcanine"],
 		desc: "If held by an Arcanine with Meteor Charge, it can use Eternal Flames.",
 	},
+
 	"voidheart": {
 		id: "voidheart",
 		name: "Void Heart",
@@ -22,6 +23,21 @@ let BattleItems = {
 		zMoveUser: ["Marshadow"],
 		gen: 7,
 		desc: "If held by a Marshadow with Sharp Shadow, it can use Embrace the Void.",
+	},
+
+	"soulorb": {
+		id: "soulorb",
+		name: "Soul Orb",
+		spritenum: 249,
+		onModifyDamage(damage, source, target, move) {
+			return this.chainModify(1.5);
+		},
+		onAfterMoveSecondarySelf(source, target, move) {
+			if (source && source !== target && move && move.category !== 'Status') {
+				this.damage(source.maxhp / 10, source, source, this.getItem('soulorb'));
+			}
+		},
+		desc: "Holder's attacks do 1.5x damage, and it loses 1/10 its max HP after the attack.",
 	},
 };
 
