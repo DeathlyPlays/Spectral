b7c38c0506bf7e06552b31bd83936d312af3fee4
Diler <diler.vinho@outlook.com>
2019-09-14 00:07:56 -0300

    Update items.js

--- data/mods/sssb/items.js
+++ data/mods/sssb/items.js
@@ -12,6 +12,17 @@ let BattleItems = {
 		zMoveUser: ["Arcanine"],
 		desc: "If held by an Arcanine with Meteor Charge, it can use Eternal Flames.",
 	},
+		"voidheart": {
+		id: "voidheart",
+		name: "Void Heart",
+		spritenum: 689,
+		onTakeItem: false,
+		zMove: "Embrace the Void",
+		zMoveFrom: "Sharp Shadow",
+		zMoveUser: ["Marshadow"],
+		gen: 7,
+		desc: "If held by a Marshadow with Sharp Shadow, it can use Embrace the Void.",
+	},
 };
 
 exports.BattleItems = BattleItems;
