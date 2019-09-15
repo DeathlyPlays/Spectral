e09b48a6aebe1df671fc99ddbd9651b58dee122b
Insist <DeathlyPlays@users.noreply.github.com>
2019-09-15 00:28:55 -0500

    SSSB: Add ~Roughskull

--- data/mods/sssb/items.js
+++ data/mods/sssb/items.js
@@ -39,6 +39,42 @@ let BattleItems = {
 		},
 		desc: "Holder's attacks do 1.5x damage, and it loses 1/10 its max HP after the attack.",
 	},
+
+	"crownoftms": {
+		id: "crownoftms",
+		name: "Crown of TMS",
+		desc: "On switch-in, the user raises its Attack or Special Attack depending on if the opponent's Defense or Special Defense is lower, and raises either Defense or Special Defense the Pokemon's highest Attack stat (Physical or Special).  At full HP, this Pokemon reduces the damage of the first hit by half.",
+		shortDesc: "Raises Atk or SpA based on lower Def, Raises Def or SpD based on higher Atk, halves damage taken if at full HP.",
+		onStart(pokemon) {
+			let totaldef = 0;
+			let totalspd = 0;
+			let totalatk = 0;
+			let totalspa = 0;
+			for (const target of pokemon.side.foe.active) {
+				if (!target || target.fainted) continue;
+				totaldef += target.getStat('def', false, true);
+				totalspd += target.getStat('spd', false, true);
+				totalatk += target.getStat('atk', false, true);
+				totalspa += target.getStat('spa', false, true);
+			}
+			if (totaldef && totaldef >= totalspd) {
+				this.boost({spa: 1});
+			} else if (totalspd) {
+				this.boost({atk: 1});
+			}
+			if (totalatk && totalatk >= totalspa) {
+				this.boost({def: 1});
+			} else if (totalspd) {
+				this.boost({spd: 1});
+			}
+		},
+		onSourceModifyDamage(damage, source, target, move) {
+			if (target.hp >= target.maxhp) {
+				this.debug('Crown of TMS weaken');
+				return this.chainModify(0.5);
+			}
+		},
+	},
 };
 
 exports.BattleItems = BattleItems;
