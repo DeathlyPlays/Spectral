ac6bc6c527c757e946fa69a2b96314555d09a7d7
Insist <DeathlyPlays@users.noreply.github.com>
2019-09-16 18:38:35 -0500

    SSSB: Add our own script function for setMove

---
+++ data/mods/sssb/scripts.js
@@ -0,0 +1,23 @@
+'use strict';
+
+/**@type {BattleScriptsData} */
+let BattleScripts = {
+	setMove(pokemon, oldMove, moveid) {
+		let index = pokemon.moves.indexOf(oldMove);
+		if (index === -1) return;
+		let move = this.getMove(moveid);
+		let sketchedMove = {
+			move: move.name,
+			id: move.id,
+			pp: move.pp,
+			maxpp: move.pp,
+			target: move.target,
+			disabled: false,
+			used: false,
+		};
+		pokemon.moveset[index] = sketchedMove;
+		pokemon.moves[index] = move.id;
+	},
+};
+
+exports.BattleScripts = BattleScripts;
