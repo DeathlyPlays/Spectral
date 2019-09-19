52d6fffb80bcf238bef1dce381b358bc2345f81b
Mewthy <31905351+Mewthy@users.noreply.github.com>
2019-09-18 23:38:05 -0400

    Fixing Unbans.

---
+++ data/mods/franticfusions/formats-data.js
@@ -0,0 +1,22 @@
+'use strict';
+
+/**@type {{[k: string]: TemplateFormatsData}} */
+let BattleFormatsData = {
+	darkrai: {
+		tier: "OU",
+	},
+  genesect: {
+		tier: "OU",
+	},
+  aegislash: {
+		tier: "OU",
+	},
+  deoxysspeed: {
+		tier: "OU",
+	},
+  deoxysdefense: {
+		tier: "OU",
+	},
+  };
+
+exports.BattleFormatsData = BattleFormatsData;
