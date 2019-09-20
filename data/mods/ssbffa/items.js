cb546153cb0ee10d1e480509d4afcee970157cc5
Insist <DeathlyPlays@users.noreply.github.com>
2019-09-20 00:12:16 -0500

    SSBFFA: Add Riot Shield

---
+++ data/mods/ssbffa/items.js
@@ -0,0 +1,19 @@
+'use strict';
+
+/**@type {{[k: string]: ItemData}} */
+let BattleItems = {
+	"riotshield": {
+		id: "riotshield",
+		name: "Riot Shield",
+		spritenum: 581,
+		fling: {
+			basePower: 80,
+		},
+		onModifySpDPriority: 1,
+		onModifySpD(spd) {
+			return this.chainModify(1.5);
+		},
+		num: -1,
+		desc: "Holder's Special Defense is 1.5x.",
+	},
+};
