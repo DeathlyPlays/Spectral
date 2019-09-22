cdfa405c6245e62f9995d85ca8a124ce13e96c25
Zach Hartin <zach.hartin@gmail.com>
2019-09-22 14:57:27 -0400

    Fix SSM

--- data/mods/ssm/random-teams.js
+++ data/mods/ssm/random-teams.js
@@ -1,8 +1,8 @@
 'use strict';
 
-/** @typedef {{[name: string]: SSBSet}} SSBSets */
+/** @typedef {{[name: string]: SSMSet}} SSMSets */
 /**
- * @typedef {Object} SSBSet
+ * @typedef {Object} SSMSet
  * @property {string} species
  * @property {string | string[]} ability
  * @property {string | string[]} item
@@ -59,10 +59,10 @@ class RandomSSMTeams extends RandomTeams {
 				}
 			}
 			let name = pool[i];
-			let ssbSet = sets[name];
+			let ssmSet = sets[name];
 			i++;
 			// Enforce typing limits
-			let types = this.getTemplate(ssbSet.species).types;
+			let types = this.getTemplate(ssmSet.species).types;
 			let rejected = false;
 			for (let type of types) {
 				if (typePool[type] === undefined) typePool[type] = 0;
@@ -80,39 +80,39 @@ class RandomSSMTeams extends RandomTeams {
 			/** @type {PokemonSet} */
 			let set = {
 				name: name,
-				species: ssbSet.species,
-				item: Array.isArray(ssbSet.item) ? this.sampleNoReplace(ssbSet.item) : ssbSet.item,
-				ability: Array.isArray(ssbSet.ability) ? this.sampleNoReplace(ssbSet.ability) : ssbSet.ability,
+				species: ssmSet.species,
+				item: Array.isArray(ssmSet.item) ? this.sampleNoReplace(ssmSet.item) : ssmSet.item,
+				ability: Array.isArray(ssmSet.ability) ? this.sampleNoReplace(ssmSet.ability) : ssmSet.ability,
 				moves: [],
-				nature: Array.isArray(ssbSet.nature) ? this.sampleNoReplace(ssbSet.nature) : ssbSet.nature,
-				gender: ssbSet.gender,
+				nature: Array.isArray(ssmSet.nature) ? this.sampleNoReplace(ssmSet.nature) : ssmSet.nature,
+				gender: ssmSet.gender,
 				evs: {hp: 0, atk: 0, def: 0, spa: 0, spd: 0, spe: 0},
 				ivs: {hp: 31, atk: 31, def: 31, spa: 31, spd: 31, spe: 31},
-				level: ssbSet.level || 100,
-				shiny: ssbSet.shiny,
+				level: ssmSet.level || 100,
+				shiny: ssmSet.shiny,
 			};
-			if (ssbSet.ivs) {
-				for (let iv in ssbSet.ivs) {
+			if (ssmSet.ivs) {
+				for (let iv in ssmSet.ivs) {
 					// IVs from the set override the default of 31, assume the hardcoded IVs are legal
 					// @ts-ignore StatsTable has no index signature
-					set.ivs[iv] = ssbSet.ivs[iv];
+					set.ivs[iv] = ssmSet.ivs[iv];
 				}
 			}
-			if (ssbSet.evs) {
-				for (let ev in ssbSet.evs) {
+			if (ssmSet.evs) {
+				for (let ev in ssmSet.evs) {
 					// EVs from the set override the default of 0, assume the hardcoded EVs are legal
 					// @ts-ignore StatsTable has no index signature
-					set.evs[ev] = ssbSet.evs[ev];
+					set.evs[ev] = ssmSet.evs[ev];
 				}
 			} else {
 				set.evs = {hp: 84, atk: 84, def: 84, spa: 84, spd: 84, spe: 84};
 			}
-			while (set.moves.length < 3 && ssbSet.moves.length > 0) {
-				let move = this.sampleNoReplace(ssbSet.moves);
+			while (set.moves.length < 3 && ssmSet.moves.length > 0) {
+				let move = this.sampleNoReplace(ssmSet.moves);
 				if (Array.isArray(move)) move = this.sampleNoReplace(move);
 				set.moves.push(move);
 			}
-			set.moves.push(ssbSet.signatureMove);
+			set.moves.push(ssmSet.signatureMove);
 			team.push(set);
 		}
 		return team;
