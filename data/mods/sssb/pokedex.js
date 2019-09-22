ff6365f6a9fe179818524e07be91472993db40e6
Mewthy <31905351+Mewthy@users.noreply.github.com>
2019-09-21 23:09:16 -0400

    SSSB: Skuntank base stat buff

--- data/mods/sssb/pokedex.js
+++ data/mods/sssb/pokedex.js
@@ -11,6 +11,10 @@ let BattlePokedex = {
 		inherit: true,
 		baseStats: {hp: 80, atk: 100, def: 115, spa: 130, spd: 115, spe: 65},
 	},
+	skuntank: {
+		inherit: true,
+		baseStats: {hp: 103, atk: 93, def: 97, spa: 71, spd: 90, spe: 90},
+	},
 };
 
 exports.BattlePokedex = BattlePokedex;
