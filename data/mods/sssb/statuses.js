2301e46597c59105cd2caef0f27b1cec9038d51b
Bamd <bamdthebamd@gmail.com>
2019-09-24 22:47:40 +0600

    SSSB: Update @Horrific17's Quotes (#68)

--- data/mods/sssb/statuses.js
+++ data/mods/sssb/statuses.js
@@ -49,13 +49,13 @@ let BattleStatuses = {
 		},
 		onFaint() {
 			this.add(`c|@Horrific17|Eaa...`);
-			this.add(`c|~Satori Komeiji|Lord Horrific!`);
+			this.add(`c|~Satori Komeiji|Horrific!`);
 			this.add(`c|@Horrific17|I let my guard down... I'm sorry...`);
-			this.add(`c|~Satori Komeiji|Oh, no! Lord Horrific! Lord Horrific!`);
+			this.add(`c|~Satori Komeiji|Oh, no! Horrific! Horrific!`);
 			this.add(`c|@Horrific17|Calm yourself, Satori...`);
 			this.add(`c|~Satori Komeiji|But... But...`);
 			this.add(`c|@Horrific17|This is no time for tears, is it? Unless... Do you plan on the two of us dying here together?`);
-			this.add(`c|~Satori Komeiji|Never! That I would never allow! You must not die, milord...`);
+			this.add(`c|~Satori Komeiji|Never! That I would never allow! You must not die...`);
 			this.add(`c|@Horrific17|And neither should you, my wife. Lend me your shoulder, would you?`);
 			this.add(`c|~Satori Komeiji|Yes, of course.`);
 		},
