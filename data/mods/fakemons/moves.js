3ba53561c677fd8a23dcdaa3e2b48b9f3e38f051
Zach Hartin <zach.hartin@gmail.com>
2019-09-24 13:59:06 -0400

    SSM: Add Abrasive Combustion

--- data/mods/fakemons/moves.js
+++ data/mods/fakemons/moves.js
@@ -6,21 +6,40 @@ let BattleMovedex = {
 	"onethousandstings": {
 		id: "onethousandstings",
 		name: "One Thousand Stings",
-		basePower: 15,
+		basePower: 18,
 		accuracy: 100,
 		category: "Physical",
-		shortDesc: "Hits 5 to 10 times.",
-		desc: "Hits 5 to 10 times.",
+		shortDesc: "Hits 3 to 8 times.",
+		desc: "Hits 3 to 8 times.",
 		pp: 10,
 		secondary: null,
 		onPrepareHit(target, source) {
 			this.add('-anim', source, 'Poison Sting', target);
 		},
 		flags: {protect: 1, mirror: 1, contact: 1},
-		multihit: [5, 10],
+		multihit: [3, 8],
 		priority: 0,
 		target: "normal",
 		type: "Bug",
+	},
+	// Camerupt
+	"abrasivecombustion": {
+		id: "abrasivecombustion",
+		name: "Abrasive Combustion",
+		basePower: 130,
+		accuracy: 70,
+		category: "Special",
+		shortDesc: "Cannot be resisted. Hits neutrally on resistant types.",
+		desc: "Cannot be resisted. Damage is dealt neutrally on types that resist the move's type.",
+		pp: 10,
+		secondary: null,
+		onPrepareHit(target, source) {
+			this.add('-anim', source, 'Searing Shot', target);
+		},
+		flags: {protect: 1, mirror: 1},
+		priority: 0,
+		target: "normal",
+		type: "Fire",
 	},
 };
 
