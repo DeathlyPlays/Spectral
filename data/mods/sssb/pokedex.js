d40523d09d2b6392e9da5ac65a186a7c024e7dac
Zakuree <zach.hartin@gmail.com>
2019-09-26 13:00:51 -0400

    SSSB: Auroura Bulk Buff

--- data/mods/sssb/pokedex.js
+++ data/mods/sssb/pokedex.js
@@ -15,6 +15,10 @@ let BattlePokedex = {
 		inherit: true,
 		baseStats: {hp: 103, atk: 93, def: 97, spa: 71, spd: 90, spe: 90},
 	},
+	castform: {
+		inherit: true,
+		baseStats: {hp: 80, atk: 70, def: 80, spa: 70, spd: 80, spe: 70},
+	},
 };
 
 exports.BattlePokedex = BattlePokedex;
